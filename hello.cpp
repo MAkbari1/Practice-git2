2507d84a780b313173ef3ad3280b443677ee2723
MAkbari1 <mahdi.akbari.4477@gmail.com>
2025-04-19 00:20:06 +0330

    Second Commit

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,12 @@ void printHello() {
     std::cout << "Hello, World" << std::endl;
 }
 
+void printStudentID() {
+    std::cout << "my student ID is : 4033633008 " << std::endl; 
+}
+
 int main() {
     printHello();
+    printStudentID();
     return 0;
 }
