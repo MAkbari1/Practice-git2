c3ff680b4043a6097565106a3dca962e84354b66
MAkbari1 <mahdi.akbari.4477@gmail.com>
2025-04-19 00:34:30 +0330

    Revert "Second Commit"
    This reverts commit 2507d84a780b313173ef3ad3280b443677ee2723.

--- hello.cpp
+++ hello.cpp
@@ -4,12 +4,7 @@ void printHello() {
     std::cout << "Hello, World" << std::endl;
 }
 
-void printStudentID() {
-    std::cout << "my student ID is : 4033633008 " << std::endl; 
-}
-
 int main() {
     printHello();
-    printStudentID();
     return 0;
 }
